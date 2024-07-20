3cc620330a5c51c44dd51513fcbb80dbe896e018
ThendoRavi <thendorav@gmail.com>
2024-07-20 22:28:18 +0200

    chore: Update deployment path in .cpanel.yml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dev_site/', // Replace with your repository name
   plugins: [react()],
-})
+});

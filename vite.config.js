eb89de9dea363d58eb37bd37d525280bdb9ac5cf
ThendoRavi <thendorav@gmail.com>
2024-07-21 18:19:21 +0200

    delete everything

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/dev_site/', // Replace with your repository name
-  plugins: [react()],
-});

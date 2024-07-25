ed5bbbbdc078e761bf10c48aea6d1a4b450e4622
ThendoRavi <thendorav@gmail.com>
2024-07-25 21:16:03 +0200

    chore: Update base path for deployment in vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/dev_site" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 export default App;

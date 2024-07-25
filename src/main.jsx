99cc219bc641ce6ee0d33fc99bd4326e94f1c735
ThendoRavi <thendorav@gmail.com>
2024-07-25 21:28:56 +0200

    chore: Update base path for deployment in vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import "./App.css";
 
+const basename = process.env.NODE_ENV === "production" ? "/dev_site" : "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/dev_site">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>

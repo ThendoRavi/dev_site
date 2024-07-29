e507f70a892254f2c3728595ecfa3f6d1616e43f
ThendoRavi <thendorav@gmail.com>
2024-07-29 16:55:01 +0200

    chore: Update base path for deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This should match your GitHub Pages repository name
+  base: '/dev_site/', // This should match your GitHub Pages repository name
 });

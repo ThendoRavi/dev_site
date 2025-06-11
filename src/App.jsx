16382298be256a2da017dfe9f027e53af81880bd
ThendoRavi <thendorav@gmail.com>
2025-06-11 21:12:02 +0200

    Remove unnecessary Router component wrapping in App and simplify routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import WorkPage from "./components/WorkPage";
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/work" element={<WorkPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/work" element={<WorkPage />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
-
 export default App;
